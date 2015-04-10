bf85474ef7b3fb496eb2f14ae1b9bfa2cad4917e
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-04-10 14:40:21 +0900

    rewrite Eratosthenes
    modified:   Haskell/Prob007.hs
    modified:   Haskell/Prob010.hs

--- Haskell/Prob007.hs
+++ Haskell/Prob007.hs
@@ -1,4 +1,3 @@
-import Control.Monad
 import Data.Array.Unboxed
 
 main :: IO ()
@@ -9,16 +8,12 @@ answer = primesTo 2000000 !! 10001
 
 primesTo :: Int -> [Int]
 primesTo n = aux 2 a0 where
-    aux :: Int -> UArray Int Bool -> [Int]
     aux i a
-        | i > n
-            = []
-        | a ! i && i * i > n
-            = i : aux (i + 1) a
+        | i * i > n
+            = filter (a !) [i .. n]
         | a ! i
             = i : aux (i + 1) a'
         | otherwise
             = aux (i + 1) a
         where a' = a // [ (i * j, False) | j <- [i .. n `div` i] ]
-    a0 :: UArray Int Bool
-    a0 = array (2, n) [ (i, True) | i <- [2 .. n] ]
+    a0 = array (2, n) [ (i, True) | i <- [2 .. n] ] :: UArray Int Bool
--- Haskell/Prob010.hs
+++ Haskell/Prob010.hs
@@ -1,4 +1,3 @@
-import Control.Monad
 import Data.Array.Unboxed
 
 main :: IO ()
@@ -9,14 +8,12 @@ answer = sum $ primesTo 2000000
 
 primesTo :: Int -> [Int]
 primesTo n = aux 2 a0 where
-    a0 = array (2, n) [ (i, True) | i <- [2 .. n] ] :: UArray Int Bool
     aux i a
-        | i > n
-            = []
-        | a ! i && i * i <= n
-            = i : aux (i + 1) a'
+        | i * i > n
+            = filter (a !) [i .. n]
         | a ! i
-            = i : aux (i + 1) a
+            = i : aux (i + 1) a'
         | otherwise
             = aux (i + 1) a
         where a' = a // [ (i * j, False) | j <- [i .. n `div` i] ]
+    a0 = array (2, n) [ (i, True) | i <- [2 .. n] ] :: UArray Int Bool
