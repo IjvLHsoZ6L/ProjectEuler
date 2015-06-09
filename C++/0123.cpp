d63c49acc930b4156fe83d7000ebdaef7040a4d5
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-06-10 01:06:07 +0900

    rewrite C++/0123.cpp

--- C++/0123.cpp
+++ C++/0123.cpp
@@ -1,45 +1,34 @@
-// 0123.cpp
 #include <iostream>
+
 using namespace std;
+
+const int PRIME = 1E6;
+
+bool prime[PRIME];
 
 int main() {
 
-    int target = 1e6;
-
-    bool prime[target];
-    prime[0] = prime[1] = false;
-    for ( int i = 2; i < target; i++ )
+    for (int i = 2; i < PRIME; i++)
         prime[i] = true;
+    for (int i = 2; i * i < PRIME; i++)
+        if (prime[i])
+            for (int j = i * i; j < PRIME; j += i)
+                prime[j] = false;
 
-    for ( int i = 2; i*i < target; i++ )
-        if ( prime[i] )
-            for ( int j = i; i*j < target; j++ )
-                prime[i*j] = false;
-
-    int p[target];
-
-    int nmax = 0;
-    for ( int i = 0; i < target; i++ )
-        if ( prime[i] )
-            p[++nmax] = i;
-
-    long long criterion = 1e10;
-    bool exceed = false;
-    for ( int n = 1; n <= nmax and not exceed; n++ ) {
-
-        long long square = (long long) p[n] * p[n];
-
-        long long term1 = 1, term2 = 1;
-        for ( int i = 0; i < n; i++ ) {
-            term1 = term1 * (p[n] - 1) % square;
-            term2 = term2 * (p[n] + 1) % square;
-        }
-
-        if ( (term1 + term2) % square > criterion ) {
-            exceed = true;
+    int n = 1;
+    int p = 2;
+    while (true) {
+        if (2L * n * p > 1E10L) {
             cout << n << endl;
+            return 0;
+        }
+        for (int i = 0; i < 2; i++) {
+            n++;
+            p++;
+            while (!prime[p])
+                p++;
         }
     }
 
-    return 0;
+    return 1;
 }
