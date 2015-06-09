bdea7a0a339d63ac5886f2f78d5661fc1b2e78d6
TOUNAI Shouta <tounai.shouta@gmail.com>
2015-06-10 00:03:58 +0900

    rewrite C++/0135.cpp

--- C++/0135.cpp
+++ C++/0135.cpp
@@ -1,24 +1,32 @@
-// 0135.cpp
 #include <iostream>
+
 using namespace std;
+
+const int LIMIT = 1E6;
+
+int n_solutions[LIMIT];
 
 int main() {
 
-    int target = 1e6;
+    for (int n = 1; n < LIMIT; n++)
+        n_solutions[n] = 0;
 
-    int answer = 0;
-    for ( int n = 1; n < target; n++ ) {
-
-        int cnt = 0;
-        for ( int d = 1; cnt <= 10 and d*d < 3*n; d++ )
-            if ( n % d == 0 and (d + n / d) % 4 == 0 )
-                cnt++;
-
-        if ( cnt == 10 )
-            answer++;
+    int n;
+    for (int m = 1; m < LIMIT; m++) {
+        for (int d = m / 4 + 1; d < m; d++) {
+            n = m * (4 * d - m);
+            if (n >= LIMIT)
+                break;
+            n_solutions[n]++;
+        }
     }
 
-    cout << answer << endl;
+    int count = 0;
+    for (int n = 1; n < LIMIT; n++)
+        if (n_solutions[n] == 10)
+            count++;
+
+    cout << count << endl;
 
     return 0;
 }
